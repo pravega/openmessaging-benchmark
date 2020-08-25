0c1c323235b53ceb5bf170e6dfcc1156e84e0344
Maria <Maria.Malygina@emc.com>
2020-08-25 17:55:50 +0300

    [DRAFT-0.0.3] hardcoded support for avro schema with user object [changed] AvroSerializerTest

--- driver-pravega/src/test/java/io/openmessaging/benchmark/driver/pravega/AvroSerializerTest.java
+++ driver-pravega/src/test/java/io/openmessaging/benchmark/driver/pravega/AvroSerializerTest.java
@@ -12,6 +12,7 @@ import io.pravega.schemaregistry.serializer.shared.impl.SerializerConfig;
 import io.pravega.schemaregistry.serializers.SerializerFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.avro.Schema;
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -20,22 +21,25 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.ByteBuffer;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class AvroSerializerTest {
 
-    @Ignore //TODO make unit test, not requiring standalone pravega running along with registry
+    //@Ignore //TODO make unit test, not requiring standalone pravega running along with registry
+    @Test
     public void testWithStandalonePravega() throws IOException, URISyntaxException {
-        runTest("src/test/resources/schema-registry/pravega-standalone.yaml", "src/test/resources/schema-registry/user-payload-100b.json");
+        runTest("src/test/resources/schema-registry/pravega-standalone.yaml", "src/test/resources/schema-registry/user-payload-100b.json", 10);
     }
 
     @Test
     public void test() throws IOException, URISyntaxException {
-        runTest("src/test/resources/schema-registry/pravega.yaml", "src/test/resources/schema-registry/user-payload-100b.json");
+        runTest("src/test/resources/schema-registry/pravega.yaml", "src/test/resources/schema-registry/user-payload-100b.json", 10);
     }
 
-    private void runTest(String driverConfigFile, String payloadFile) throws IOException, URISyntaxException {
+    private void runTest(String driverConfigFile, String payloadFile, int serializeTimes) throws IOException, URISyntaxException {
         File driverfile = new File(driverConfigFile);
         PravegaConfig driverConfig = PravegaBenchmarkDriver.readConfig(driverfile);
         SchemaRegistryConfig schemaRegistryConfig = driverConfig.schemaRegistry;
@@ -68,17 +72,21 @@ public class AvroSerializerTest {
         ObjectMapper mapper = new ObjectMapper();
         User user = mapper.readValue(new File(payloadFile), User.class);
         user.setEventTimestamp(System.currentTimeMillis());
-        long before = System.nanoTime();
-        ByteBuffer serialized = serializer.serialize(user);
-        double serializeMillis = (System.nanoTime() - before)/(double) TimeUnit.MILLISECONDS.toNanos(1);
-        int payloadSize = serialized.array().length;
-        log.info("Serialize payload size: {} bytes User object in {} millis", payloadSize, serializeMillis);
-        log.info("Payload size: {} bytes", payloadSize);
-        before = System.nanoTime();
-        User deserializedUser = (User) deserializer.deserialize(serialized);
-        long deserializeNanos = System.nanoTime() - before;
-        double deserializeMillis = deserializeNanos/(double) TimeUnit.MILLISECONDS.toNanos(1);
-        log.info("Deserialized user: {}",  deserializedUser);
-        log.info("Deserialize payload {} bytes User object in {} nanos {} millis", payloadSize, deserializeNanos, deserializeMillis);
+        List<Pair<Double, Double>> serialize = new LinkedList<>();
+        int payloadSize = 0;
+        for (int i = 0; i < serializeTimes; i++) {
+            long before = System.nanoTime();
+            ByteBuffer serialized = serializer.serialize(user);
+            double serializeMillis = (System.nanoTime() - before) / (double) TimeUnit.MILLISECONDS.toNanos(1);
+            payloadSize = serialized.array().length;
+            before = System.nanoTime();
+            User deserializedUser = (User) deserializer.deserialize(serialized);
+            long deserializeNanos = System.nanoTime() - before;
+            double deserializeMillis = deserializeNanos / (double) TimeUnit.MILLISECONDS.toNanos(1);
+            //log.info("Deserialized user: {}", deserializedUser);
+            log.info("Payload {} bytes, serialize: {}ms, deserialize: {}ms", payloadSize, deserializeMillis, deserializeMillis);
+            serialize.add(Pair.of(serializeMillis, deserializeMillis));
+        }
+
     }
 }
